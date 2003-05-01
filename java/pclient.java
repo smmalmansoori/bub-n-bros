292fef20eb54bad362e9e840721af0845f80c6d3
Armin Rigo <arigo@tunes.org>
2003-05-01 15:15:24 +0000

    force keyboard focus (needed for Mozilla/Linux)

--- java/pclient.java
+++ java/pclient.java
@@ -1048,6 +1048,11 @@ public class pclient extends Applet {
     }
 
     protected void processMouseEvent(MouseEvent e) {
+        if (e.getID() != MouseEvent.MOUSE_PRESSED) {
+            e.consume();
+            return;
+        }
+        requestFocus();
         keydefinition_k = null;
         Playfield pf = playfield;
         if (pf != null && e.getY() >= pf.pfheight - pf.TASKBAR_HEIGHT) {
