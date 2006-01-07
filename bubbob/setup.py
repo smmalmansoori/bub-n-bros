96ae002a9d3180c484016cc1de32b1c8aea209db
Armin Rigo <arigo@tunes.org>
2006-01-07 09:44:20 +0000

    Untabify.

--- bubbob/setup.py
+++ bubbob/setup.py
@@ -3,20 +3,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-##setup (	name="gencopy",
-##      	version="0.1",
-##      	description="generator and iterator state",
-##      	author="Armin",
+##setup (       name="gencopy",
+##              version="0.1",
+##              description="generator and iterator state",
+##              author="Armin",
 ##        author_email="arigo@tunes.org",
-##      	ext_modules=[Extension(name = 'gencopy',
+##              ext_modules=[Extension(name = 'gencopy',
 ##                               sources = ['gencopy.c'])]
 ##        )
 
-setup (	name="statesaver",
-      	version="0.1",
-      	description="object duplicator working on generators and iterators",
-      	author="Armin",
+setup ( name="statesaver",
+        version="0.1",
+        description="object duplicator working on generators and iterators",
+        author="Armin",
         author_email="arigo@tunes.org",
-      	ext_modules=[Extension(name = 'statesaver',
+        ext_modules=[Extension(name = 'statesaver',
                                sources = ['statesaver.c'])]
         )
