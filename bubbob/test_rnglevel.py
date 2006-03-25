5b5ea998df9d36dba67be50b19ea3113d1f4453c
pkalli <>
2006-03-25 14:04:11 +0000

    improve random level test program to optionally output the level walls and winds.

--- bubbob/test_rnglevel.py
+++ bubbob/test_rnglevel.py
@@ -4,17 +4,46 @@
 # possible (in the limited sense of not having any full-
 # column walls)
 #
+# this test accepts the following parameters:
+# -wall    show the level layout
+# -wind    show the level wind pattern
+#
 
 import sys
 sys.path.append('..')
 sys.path.append('../common')
 
-for i in range(100):
+n_lvls = 100
+show_lvl = 0
+
+for arg in sys.argv:
+    if arg == '-wall':
+        show_lvl |= 1
+        n_lvls = 2
+    if arg == '-wind':
+        show_lvl |= 2
+        n_lvls = 2
+
+def printlvl(level):
+    if show_lvl:
+        print "\n\n"
+        for y in range(level.HEIGHT):
+            str = ""
+            if show_lvl & 1:
+                str = level.walls[y]
+            if show_lvl & 2:
+                if str:
+                    str += " | "
+                str += level.winds[y]
+            print str
+
+for i in range(n_lvls):
     print '%4d:' % i,
     d = {'__name__': 'RandomLevels'}
     execfile('levels/RandomLevels.py', d)
     for i, Lvl in enumerate(d['Levels']):
         level = Lvl(i)
+        printlvl(level)
         for x in range(2, level.width-2):
             for y in range(0, level.height):
                 if level.walls[y][x] == ' ':
